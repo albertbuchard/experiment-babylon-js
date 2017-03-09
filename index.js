479547923c67c57394251417c211b41de123f5af
albert.buchard <antinabi87>
2017-03-09 20:48:17 +0100

    First commit
    Bundled Babylon 2.5 3d and 2d along with Oimo and handjs - made it
    importable

---
+++ index.js
@@ -0,0 +1,5 @@
+import { BABYLON } from './src/babylon.2.5.full.max'
+import { OIMO } from './src/Oimo'
+import { HANDJS } from './src/Hand'
+
+export { BABYLON, OIMO, HANDJS }
---
+++ webpack.config.babel.js
@@ -0,0 +1,41 @@
+// @flow
+
+import path from 'path'
+import webpack from 'webpack'
+
+const WDS_PORT = 7000
+
+const PROD = JSON.parse(process.env.PROD_ENV || '0')
+
+const plugins = [
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NamedModulesPlugin(),
+  new webpack.NoEmitOnErrorsPlugin(),
+]
+const prodPlugins = plugins.concat(new webpack.optimize.UglifyJsPlugin())
+
+export default {
+  entry: [
+    './index.js',
+  ],
+  output: {
+    filename: PROD ? 'babylon.min.js' : 'babylon.max.js',
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: `http://localhost:${WDS_PORT}/dist/`,
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  devtool: PROD ? false : 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: WDS_PORT,
+    hot: true,
+  },
+  plugins: PROD ? prodPlugins : plugins,
+}
